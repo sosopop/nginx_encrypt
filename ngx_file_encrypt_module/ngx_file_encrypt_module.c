84758157a80942aedea56424be12e911d5cda633
sosopop <cabbage1102@gmail.com>
2019-04-25 01:33:20 +0800

    临时保存开发内容

--- ngx_file_encrypt_module/ngx_file_encrypt_module.c
+++ ngx_file_encrypt_module/ngx_file_encrypt_module.c
@@ -1,75 +1,183 @@
-#include "ngx_core.h"
-#include "ngx_string.h"
-#include "ngx_buf.h"
-#include "ngx_module.h"
-#include "ngx_conf_file.h"
-#include "ngx_http.h"
-#include "ngx_http_request.h"
-#include "ngx_http_config.h"
-#include "ngx_http_core_module.h"
 
-// 返回值如NGX_HTTP_OK（200）、NGX_HTTP_NOT_FOUND（404）、NGX_HTTP_INTERNAL_SERVER_ERROR（500）、NGX_OK（0）、NGX_ERROR（-1）等
-static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ */
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+
+typedef struct {
+    ngx_str_t     encrypt_key;
+} ngx_file_encrypt_conf_t;
+
+
+typedef struct {
+} ngx_file_encrypt_ctx_t;
+
+
+static void *ngx_file_encrypt_create_conf(ngx_conf_t *cf);
+static char *ngx_file_encrypt_merge_conf(ngx_conf_t *cf, void *parent,
+    void *child);
+static ngx_int_t ngx_file_encrypt_filter_init(ngx_conf_t *cf);
+
+
+static ngx_command_t  ngx_file_encrypt_commands[] = {
+
+    { ngx_string("encrypt_key"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_str_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_file_encrypt_conf_t, encrypt_key),
+      NULL },
+      ngx_null_command
+};
+
+
+static ngx_http_module_t  ngx_file_encrypt_module_ctx = {
+    NULL,                                  /* preconfiguration */
+    ngx_file_encrypt_filter_init,         /* postconfiguration */
+
+    NULL,                                  /* create main configuration */
+    NULL,                                  /* init main configuration */
+
+    NULL,                                  /* create server configuration */
+    NULL,                                  /* merge server configuration */
+
+    ngx_file_encrypt_create_conf,         /* create location configuration */
+    ngx_file_encrypt_merge_conf           /* merge location configuration */
+};
+
+
+ngx_module_t  ngx_file_encrypt_module = {
+    NGX_MODULE_V1,
+    &ngx_file_encrypt_module_ctx,  /* module context */
+    ngx_file_encrypt_commands,            /* module directives */
+    NGX_HTTP_MODULE,                       /* module type */
+    NULL,                                  /* init master */
+    NULL,                                  /* init module */
+    NULL,                                  /* init process */
+    NULL,                                  /* init thread */
+    NULL,                                  /* exit thread */
+    NULL,                                  /* exit process */
+    NULL,                                  /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+
+static ngx_http_output_body_filter_pt    ngx_http_next_body_filter;
+
+
+static ngx_int_t
+ngx_http_encrypt_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
-    if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD)))
-        return NGX_HTTP_NOT_ALLOWED;
+    ngx_int_t                  rc;
+    ngx_uint_t                 last;
+    ngx_chain_t               *cl;
+    ngx_http_request_t        *sr;
+    ngx_file_encrypt_ctx_t   *ctx;
+    ngx_file_encrypt_conf_t  *conf;
 
-    ngx_int_t rc = ngx_http_discard_request_body(r);
-    if (rc != NGX_OK)
+    if (in == NULL || r->header_only) {
+        return ngx_http_next_body_filter(r, in);
+    }
+
+    ctx = ngx_http_get_module_ctx(r, ngx_file_encrypt_module);
+
+    if (ctx == NULL) {
+        return ngx_http_next_body_filter(r, in);
+    }
+
+    conf = ngx_http_get_module_loc_conf(r, ngx_file_encrypt_module);
+/*
+    if (!ctx->before_body_sent) {
+        ctx->before_body_sent = 1;
+
+        if (conf->before_body.len) {
+            if (ngx_http_subrequest(r, &conf->before_body, NULL, &sr, NULL, 0)
+                != NGX_OK)
+            {
+                return NGX_ERROR;
+            }
+        }
+    }
+
+    if (conf->after_body.len == 0) {
+        ngx_http_set_ctx(r, NULL, ngx_file_encrypt_module);
+        return ngx_http_next_body_filter(r, in);
+    }
+
+    last = 0;
+*/
+    for (cl = in; cl; cl = cl->next) {
+        if (cl->buf->last_buf) {
+            cl->buf->last_buf = 0;
+            cl->buf->last_in_chain = 1;
+            cl->buf->sync = 1;
+            last = 1;
+        }
+    }
+
+    rc = ngx_http_next_body_filter(r, in);
+
+    if (rc == NGX_ERROR || !last || conf->after_body.len == 0) {
         return rc;
+    }
 
-    ngx_str_t type = ngx_string("text/plain");
-    ngx_str_t response = ngx_string("Hello World!\n");
-    r->headers_out.status = NGX_HTTP_OK;
-    r->headers_out.content_length_n = response.len;
-    r->headers_out.content_type = type;
+    if (ngx_http_subrequest(r, &conf->after_body, NULL, &sr, NULL, 0)
+        != NGX_OK)
+    {
+        return NGX_ERROR;
+    }
 
-    /* 发送HTTP响应时需要执行发送HTTP头部（和响应行）和发送HTTP包体两步操作，此为第一步
-       该函数会首先调用所有的HTTP过滤模块共同处理r->headers_out中定义的HTTP响应头部，全部处理完毕后才会序列化为TCP字节流发送到客户端 */
-    rc = ngx_http_send_header(r);
-    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
-        return rc;
+    ngx_http_set_ctx(r, NULL, ngx_file_encrypt_module);
 
-    ngx_buf_t *b;
-    b = ngx_create_temp_buf(r->pool, response.len);
-    if (b == NULL)
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-
-    ngx_memcpy(b->pos, response.data, response.len);
-    b->last = b->pos + response.len;
-    b->last_buf = 1;
-
-    ngx_chain_t out;
-    out.buf = b;
-    out.next = NULL;
-
-    return ngx_http_output_filter(r, &out);  // 发送HTTP响应包体
+    return ngx_http_send_special(r, NGX_HTTP_LAST);
 }
 
-static char *ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+
+static ngx_int_t
+ngx_file_encrypt_filter_init(ngx_conf_t *cf)
 {
-    ngx_http_core_loc_conf_t *clcf;
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_mytest_handler;  // HTTP框架接收完HTTP请求的头部后，将调用该函数
+    ngx_http_next_body_filter = ngx_http_top_body_filter;
+    ngx_http_top_body_filter = ngx_http_encrypt_body_filter;
+    return NGX_OK;
+}
+
+
+static void *
+ngx_file_encrypt_create_conf(ngx_conf_t *cf)
+{
+    ngx_file_encrypt_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_file_encrypt_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    /*
+     * set by ngx_pcalloc():
+     *
+     *     conf->before_body = { 0, NULL };
+     *     conf->after_body = { 0, NULL };
+     *     conf->types = { NULL };
+     *     conf->types_keys = NULL;
+     */
+
+    return conf;
+}
+
+
+static char *
+ngx_file_encrypt_merge_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+    ngx_file_encrypt_conf_t *prev = parent;
+    ngx_file_encrypt_conf_t *conf = child;
+
+    ngx_conf_merge_str_value(conf->encrypt_key, prev->encrypt_key, "test");
 
     return NGX_CONF_OK;
 }
-
-static ngx_command_t ngx_file_encrypt_commands[] = {
-    {ngx_string("tryconf"), NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF |
-        NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS, ngx_http_mytest, NGX_HTTP_LOC_CONF_OFFSET, 0, NULL},
-    ngx_null_command
-};
-
-static ngx_http_module_t ngx_file_encrypt_module_ctx = {
-    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL
-};
-
-ngx_module_t ngx_file_encrypt_module = {
-    NGX_MODULE_V1,
-    &ngx_file_encrypt_module_ctx,
-    ngx_file_encrypt_commands,
-    NGX_HTTP_MODULE,
-    NULL, NULL, NULL, NULL, NULL, NULL, NULL,
-    NGX_MODULE_V1_PADDING
-};
